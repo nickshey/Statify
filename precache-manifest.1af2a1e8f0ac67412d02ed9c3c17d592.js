31383ccb91bcf53db2077d63b6cc8a55b25970c8
Nojan Sheybani <nojan@Nojans-MBP.frontierlocal.net>
2019-01-07 15:18:56 -0500

    Updates

---
+++ precache-manifest.1af2a1e8f0ac67412d02ed9c3c17d592.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1798859a2b3306f3870ad3e73c232e70",
+    "url": "/myspotify/static/media/twitter.1798859a.png"
+  },
+  {
+    "revision": "8fb1e46bef09a9327504bca251133025",
+    "url": "/myspotify/static/media/spotify.8fb1e46b.png"
+  },
+  {
+    "revision": "019803bbdcc0cc9bcd9c8bb0026f27e6",
+    "url": "/myspotify/static/media/login.019803bb.svg"
+  },
+  {
+    "revision": "77dd69756fb75b65748b",
+    "url": "/myspotify/static/js/runtime~main.77dd6975.js"
+  },
+  {
+    "revision": "df56e1989f2e8d9a80f7",
+    "url": "/myspotify/static/js/main.df56e198.chunk.js"
+  },
+  {
+    "revision": "46e38b55b9ac454af954",
+    "url": "/myspotify/static/js/1.46e38b55.chunk.js"
+  },
+  {
+    "revision": "df56e1989f2e8d9a80f7",
+    "url": "/myspotify/static/css/main.5ad68c94.chunk.css"
+  },
+  {
+    "revision": "acabd0e7174450167adeb11448ed5fcc",
+    "url": "/myspotify/index.html"
+  }
+];
