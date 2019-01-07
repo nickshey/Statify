da56779890f45762f917eb48be3aece1678321e1
Nojan Sheybani <nojan@Nojans-MBP.frontierlocal.net>
2019-01-07 16:22:11 -0500

    Deployed

---
+++ app/build/precache-manifest.c3856e82249ca0391d708048a3d732a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1798859a2b3306f3870ad3e73c232e70",
+    "url": "/static/media/twitter.1798859a.png"
+  },
+  {
+    "revision": "8fb1e46bef09a9327504bca251133025",
+    "url": "/static/media/spotify.8fb1e46b.png"
+  },
+  {
+    "revision": "019803bbdcc0cc9bcd9c8bb0026f27e6",
+    "url": "/static/media/login.019803bb.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ec09f8544f1ade9c243",
+    "url": "/static/js/main.1ec09f85.chunk.js"
+  },
+  {
+    "revision": "46e38b55b9ac454af954",
+    "url": "/static/js/1.46e38b55.chunk.js"
+  },
+  {
+    "revision": "1ec09f8544f1ade9c243",
+    "url": "/static/css/main.5ad68c94.chunk.css"
+  },
+  {
+    "revision": "ae111aa854577ab33e8251d49a7cac21",
+    "url": "/index.html"
+  }
+];
